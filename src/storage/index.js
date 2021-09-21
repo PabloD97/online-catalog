5d95d3519e9db265c5a3c618bfa59c6823e168b3
PabloD97 <pablo97758@gmail.com>
2021-09-21 14:22:15 -0300

    chore: Images
    - Se agregaron imagenes de los productos a la carpeta images
    - Se agregaron las imagenes a index.js de storage

--- src/storage/index.js
+++ src/storage/index.js
@@ -1,5 +1,13 @@
 import alfajor_fernet from '../images/alfajor-fernet-nevado.jpg';
 import alfajor_mani_marroc from '../images/alfajor-de-mani-marroc.jpg';
+import lemon_pie from "../images/lemon-pie.jpg";
+import masas_finas from "../images/masas-finas.jpg";
+import postre_oreo from "../images/postre-oreo.jpg";
+import postre_oreo_chico from "../images/postre-oreo-chico.jpg";
+import cheescake from "../images/cheescake.jpg";
+import postre_bon_o_bon from "../images/postre-bon-o-bon.jpg";
+import postre_bon_o_bon_chico from "../images/postre-bon-o-bon-chico.jpg";
+import masas_secas from "../images/masas-secas.jpg";
 
 /**
  * Formato de json
@@ -27,20 +35,72 @@ const alfajores = [
 ]
 
 
-const tartas = {
+const tartas = [
+    {
+        'name': 'Lemon pie',
+        'image': lemon_pie,
+        'price': 0,
+        'description': 'Lemon pie. Contiene crema de limon y merengue '
+    }
+]
 
-}
+const masas = [
+    {
+        'name': 'Masas finas',
+        'image': masas_finas,
+        'price': 0,
+        'description': 'Masitas finas en caja'
+    },
+    {
+        'name': 'Masas Secas',
+        'image': masas_secas,
+        'price': 0,
+        'description': 'Masitas secas'
+    }
+]
 
-const masas = {
 
-}
 
-const postres = {
-    
-}
+const postres = [
+    {
+        'name': 'Postre oreo Grande',
+        'image': postre_oreo,
+        'price': 0,
+        'description': 'Postre oreo en vaso grande. Contiene crema oreo, dulce de leche y pedazos de galletita oreo'
+    },
+    {
+        'name': 'Postre oreo Chico',
+        'image': postre_oreo_chico,
+        'price': 0,
+        'description': 'Postre oreo en vaso chico. Contiene crema oreo, dulce de leche y pedazos de galletita oreo'
+    },
+    {
+        'name': 'Cheescake chico',
+        'image': cheescake,
+        'price': 0,
+        'description': 'Postre cheescake en vaso chico. Contiene crema cheescake, masa de galletitas y mermelada de frutos rojos'
+    },
+    {
+        'name': 'Postre bon o bon Grande',
+        'image': postre_bon_o_bon,
+        'price': 0,
+        'description': 'Postre bon o bon en vaso grande. Contiene crema de dll y pedazos de bon o bon'
+    },
+    {
+        'name': 'Postre bon o bon chico',
+        'image': postre_bon_o_bon_chico,
+        'price': 0,
+        'description': 'Postre bon o bon en vaso grande. Contiene crema de dll y pedazos de bon o bon'
+    },
+]
 
-const tortas = {
-
-}
+const tortas = [
+    {
+        'name': 'Chocotorta',
+        'image': postre_oreo,
+        'price': 0,
+        'description': 'Chocotorta de 24cm'
+    }
+]
 
 export {alfajores, tartas, masas, postres, tortas};
